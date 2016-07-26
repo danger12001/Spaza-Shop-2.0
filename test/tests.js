b55d21cfc5bee4b06d893accf4bad2eca3762ea3
danger12001 <paul@projectcodex.co>
2016-07-26 10:15:31 +0200

    Trying to get env to work for password

--- test/tests.js
+++ test/tests.js
@@ -3301,7 +3301,7 @@ describe('ProductsDataService', function() {
   var dbOptions = {
     host: '127.0.0.1',
     user: process.env.MYSQL_USER || 'root',
-    password: null || '5550121a',
+    password: password,
     port: 3306,
     database: "travis_db"
   };
